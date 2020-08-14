ead571c4e41f9c59f18065b5b9b1740ef8f81548
Stacey Louis <stacey-louis@lambdastudents.com>
2020-08-14 11:19:29 -0400

    initial commit

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,23 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import {Provider} from 'react-redux'
+import thunk from 'thunk'
+import reducer from './reducers/reducer'
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+function App() {
+    return (
+        <div>
+            <h1> Smurfs </h1>
+            <div></div>
+        </div>
+    )
+}
+
+ReactDOM.render(<Provider store ={store}><App/></Provider>, document.getElementById("root"));
